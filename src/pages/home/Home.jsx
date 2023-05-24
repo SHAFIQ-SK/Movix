9ce5a825a1adb8f223fe34d8414158a4c54f8815
Shafiq <shafiquddinshaikh96@gmail.com>
2023-05-24 15:29:18 +0530

    header scroll effect & footer trending component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import "./style.scss";
 import Homebanner from "./homeBanner/Homebanner";
+import Trending from "./trending/Trending";
+// import Footer from "../../components/footer/Footer";
+
 const Home = () => {
   return (
     <div className="homePage">
       {" "}
       <Homebanner />
+      <Trending />
+      <div style={{ height: 1000 }}>asds</div>
+      {/* <Footer /> */}
     </div>
   );
 };
