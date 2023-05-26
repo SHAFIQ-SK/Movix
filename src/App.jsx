53190208aa84fc5fc11e5ce6176bf7d91c8ed299
Shafiq <shafiquddinshaikh96@gmail.com>
2023-05-26 15:46:53 +0530

    Details page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,68 @@
 import { useEffect } from "react";
-import { fetchDataFromApi } from "./utils/api";
-import { useDispatch, useSelector } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+import SearchPage from "./pages/searchResult/SearchPage";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import Explore from "./pages/explore/Explore";
-import SearchPage from "./pages/searchResult/SearchPage";
 import PageNotFound from "./pages/404/PageNotFound";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+
 function App() {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
+  console.log(url);
 
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, []);
 
-  const fetchApiConfig = async () => {
-    // fetchDataFromApi("/movie/popular").then((res) => {
-    //   console.log(res);
-    // });
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
 
-    let res = await fetchDataFromApi("/configuration");
-    const data = await res;
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
 
-    const url = {
-      backdrop: data.images.secure_base_url + "original",
-      poster: data.images.secure_base_url + "original",
-      profile: data.images.secure_base_url + "original",
-    };
-
-    dispatch(getApiConfiguration(url));
+      dispatch(getApiConfiguration(url));
+    });
   };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchPage />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="/search/:query" element={<SearchPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import homeSlice from "./homeSlice";
 
 export const store = configureStore({
